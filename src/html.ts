da8de3a3eba4cdbbc202ef8d6e1d86e5dcd76b0a
shortercode <iainshorter@gmail.com>
2022-07-04 22:09:09 +0100

    slight refactor of template parsing

--- src/html.ts
+++ src/html.ts
@@ -1,6 +1,5 @@
 import type { Value } from './Child.type';
 import { HTML_NAMESPACE, SVG_NAMESPACE } from './Namespace.constants';
-import type { Namespace } from './Namespace.type';
 import { close_parser, create_parser, parse_chunk } from './parser';
 import { render_template } from './render';
 import type { Template } from './Template.type';
@@ -8,27 +7,29 @@ import type { Template } from './Template.type';
 const template_cache = new Map<TemplateStringsArray, Template>();
 
 export function svg (literal: TemplateStringsArray, ...parts: Value[]): DocumentFragment | Element {
-  return helium(literal, parts, SVG_NAMESPACE);
+  const template = parse_template(literal);
+  return render_template(template, parts, SVG_NAMESPACE);
 }
 
 export function html (literal: TemplateStringsArray, ...parts: Value[]): DocumentFragment | Element {
-  return helium(literal, parts, HTML_NAMESPACE);
+  const template = parse_template(literal);
+  return render_template(template, parts, HTML_NAMESPACE);
 }
 
-export function helium (literal: TemplateStringsArray, parts: Value[], namespace: Namespace): DocumentFragment | Element {
-  let template = template_cache.get(literal);
+export function parse_template (literal: TemplateStringsArray): Template {
+  const template = template_cache.get(literal);
 
-  // first part can memoised based on the literal
-  if (!template) {
-    const ctx = create_parser();
-    const l = parts.length;
-    for (const [i, chunk] of literal.raw.entries()) {
-      // NOTE no associated part with the last chunk, so pass an index of -1
-      parse_chunk(ctx, chunk, i < l ? i : - 1);
-    }
-    template = close_parser(ctx);
-  } 
+  if (template) {
+    return template;
+  }
 
-  // this bit needs to be done fresh each time
-  return render_template(template, parts, namespace);
+  const ctx = create_parser();
+  const l = literal.length - 1;
+
+  for (const [i, chunk] of literal.raw.entries()) {
+    // NOTE no associated part with the last chunk, so pass an index of -1
+    parse_chunk(ctx, chunk, i < l ? i : - 1);
+  }
+	
+  return close_parser(ctx);
 }
