2cdfc5087bef0f93c71304f5c5cf21e999e7842d
shortercode <iainshorter@gmail.com>
2022-08-01 09:05:08 +0100

    Define FoundationObservable

--- src/Observable/Observable.type.ts
+++ src/Observable/Observable.type.ts
@@ -1,10 +1,12 @@
-export interface Observable<T> {
+export interface FoundationObservable<T> {
   watch(next: Listener<T>): Disposable;
+}
+
+export interface Observable<T> extends FoundationObservable<T> {
   pipe<R> (operator: Operator<T, R>): Observable<R>;
   map<R> (mapper: (value: T) => R): Observable<R>;
   filter<R extends T> (predicate: (value: T) => value is R): Observable<R>;
   filter(predicate: (value: T) => boolean): Observable<T>;
-  view<K extends keyof T> (key: K): Observable<T[K]>
 }
 
 export interface Disposable {
@@ -13,4 +15,4 @@ export interface Disposable {
 
 export type Listener<T> = (value: T) => void;
 export type Subscriber<T> = (listener: Listener<T>) => Disposable;
-export type Operator<T, R = T> = (source: Observable<T>) => Observable<R>;
+export type Operator<T, R = T> = (source: FoundationObservable<T>) => Observable<R>;
